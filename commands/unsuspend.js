434ef592477970035f8dbe29fcf02f10d6adcdd2
Chujia Guo Local <herobrinevinyl@gmail.com>
2020-04-23 19:56:48 -0400

    Quick bug fix.

--- commands/unsuspend.js
+++ commands/unsuspend.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 exports.run = async (client, message, args, Discord, sudo = false) => {
     if (!sudo) {
         let commandFile = require(`./permcheck.js`);
-        var auth = await commandFile.run(client, message.member, 10000)
+        var auth = await commandFile.run(client, message.member, 1000)
         if (!auth) {
             return message.channel.send("You do not have permission to use this command.")
         }
