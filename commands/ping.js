352c42d755cfbf95c115b384bcb29c81e09b72c8
Chujia Guo Local <herobrinevinyl@gmail.com>
2020-05-03 14:52:20 -0400

    Made ping look better

--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,11 @@
 exports.run = async (client, message, args, Discord, sudo = false) => {
     const m = await message.channel.send("Ping?");
-    m.edit(`Bot latency is \`${m.createdTimestamp - message.createdTimestamp}\` ms. API Latency is \`${Math.round(client.ws.ping)}\` ms.`);
+    let embed = new Discord.MessageEmbed()
+    .setTitle("Bot Status")
+    .setColor("#30ffea")
+    .addField("Bot Latency:", m.createdTimestamp-message.createdTimestamp)
+    .addField("API Latency:", Math.round(client.ws.ping))
+    .addField("Uptime:", `${Math.floor(client.uptime/86400000)} Days ${Math.floor((client.uptime - Math.floor(client.uptime/86400000) * 86400000) / 3600000)} Hours ${Math.round((client.uptime - Math.floor(client.uptime/86400000) * 86400000 - Math.floor((client.uptime - Math.floor(client.uptime/86400000) * 86400000) / 3600000) * 3600000) / 60000)} Minutes`)
+    await m.delete();
+    message.channel.send(embed)
 }
