fedb845ab190258bb85e35d482cb97989ef6c0e1
Chujia Guo Local <herobrinevinyl@gmail.com>
2020-06-06 15:57:57 -0400

    Updated commands command to be self updating with permissions

--- commands/commands.js
+++ commands/commands.js
@@ -11,6 +11,12 @@ exports.run = async (client, message, args, Discord) => {
             for(var x in categories){
                 let type = categories[x]
                 sorted[type] = Object.entries(commands.settings).filter(cmd => cmd.length > 1 && cmd[1].category != undefined && cmd[1].category.toLowerCase() == type)
+                for(var y in sorted[type]){
+                    let auth = await permcheck.run(client, message.member, sorted[type][y][1].permsint == "0"? message.guild.id:sorted[type][y][1].permsint)
+                    if(!auth){
+                        sorted[type].splice(sorted[type].indexOf(y), 1)
+                    }
+                }
             }
             var descriptionString = ""
             for(var x in sorted){
