8011a77cbfcb257893251c31d53c63dcf22f7fc7
Chujia Guo Local <herobrinevinyl@gmail.com>
2020-06-22 18:06:47 -0400

    Duplicateroles faster

--- commands/duplicateroles.js
+++ commands/duplicateroles.js
@@ -4,10 +4,13 @@ exports.run = async (client, message, args, Discord, sudo = false) => {
     const config = JSON.parse(fs.readFileSync("config.json"))
     let eventRole = message.guild.roles.cache.get(config.roles.general.eventraider)
     let raiderRole = message.guild.roles.cache.get(config.roles.general.raider)
+    let raiders = raiderRole.members.map(m => m.id)
+    let eraiders = eventRole.members.map(e => e.id)
     if (!eventRole || !raiderRole) {
         return message.channel.send("One or the other role is undefined.")
     }
-    let both = raiderRole.members.map(m => m).filter(m => eventRole.members.map(e => e.id).includes(m.id))
+    let both = raiders.filter(m => eraiders.includes(m))
+    both = [... new Set(both)].map(m => `<@!${m}>`)
     let duplicateEmbed = new Discord.MessageEmbed()
         .setColor(raiderRole.hexColor)
         .setDescription(`The following people have both ${eventRole} and ${raiderRole}:\n${both.join(", ")}`)
