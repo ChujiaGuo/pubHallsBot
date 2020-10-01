ec26c427d8521549e5a33b77ce37dac6a5276e48
Chujia Guo Local <herobrinevinyl@gmail.com>
2020-10-01 19:58:09 -0400

    added a few more rejects

--- commands/parsemembers.js
+++ commands/parsemembers.js
@@ -42,34 +42,42 @@ exports.run = async (client, message, args, Discord, sudo = false) => {
             } else {
                 if (origin == 100) {
                     if (config.channels.veteran.raiding[channelNumber] == undefined) {
+                        reject()
                         return message.channel.send(`\`${channelNumber}\` is an invalid channel number.`)
                     }
                     if (config.channels.veteran.raiding[channelNumber].length == 0) {
+                        reject()
                         return message.channel.send(`Raiding Channel \`${channelNumber}\` has not been configured. Please have an admin add it using setup.`)
                     } else {
                         channelNumber = config.channels.veteran.raiding[channelNumber]
                     }
                 } else if (origin == 10) {
                     if (config.channels.normal.raiding[channelNumber] == undefined) {
+                        reject()
                         return message.channel.send(`\`${channelNumber}\` is an invalid channel number.`)
                     }
                     if (config.channels.normal.raiding[channelNumber].length == 0) {
+                        reject()
                         return message.channel.send(`Raiding Channel \`${channelNumber}\` has not been configured. Please have an admin add it using setup.`)
                     } else {
                         channelNumber = config.channels.normal.raiding[channelNumber]
                     }
                 } else if (origin = 1) {
                     if (config.channels.event.raiding[channelNumber] == undefined) {
+                        reject()
                         return message.channel.send(`\`${channelNumber}\` is an invalid channel number.`)
                     }
                     if (config.channels.event.raiding[channelNumber].length == 0) {
+                        reject()
                         return message.channel.send(`Raiding Channel \`${channelNumber}\` has not been configured. Please have an admin add it using setup.`)
                     }
                     else {
+                        reject()
                         return message.channel.send("Events aren't supported yet. Sorry.")
                         channelNumber = config.channels.event.raiding[channelNumber]
                     }
                 } else {
+                    reject()
                     return message.channel.send("You should not be here.")
                 }
                 imageURL = args[1]
@@ -78,6 +86,7 @@ exports.run = async (client, message, args, Discord, sudo = false) => {
             //Fetch channel
             var raidingChannel = await message.guild.channels.cache.find(c => c.id == channelNumber)
             if (!raidingChannel) {
+                reject()
                 return message.channel.send("Invalid Channel")
             }
             var channelMembers = raidingChannel.members
@@ -86,6 +95,7 @@ exports.run = async (client, message, args, Discord, sudo = false) => {
             if (message.attachments.size == 1 || imageURL) {
                 imageURL = message.attachments.map(a => a.proxyURL)[0] || imageURL
             } else {
+                reject()
                 return message.channel.send("Please attach a single image, either as an URL or as a raw image.")
             }
             var statusEmbed = new Discord.MessageEmbed()
