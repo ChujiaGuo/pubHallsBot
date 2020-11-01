e8ff3e503b677b9b657bc52eadf74edf5805f099
Chujia Guo Local <herobrinevinyl@gmail.com>
2020-10-31 20:49:13 -0400

    Added thing to sudo

--- commands/sudo.js
+++ commands/sudo.js
@@ -1,19 +1,34 @@
 const fs = require('fs')
 
 exports.run = async (client, message, args, Discord) => {
-    try{
+    try {
         const config = JSON.parse(fs.readFileSync('config.json'))
         const commands = JSON.parse(fs.readFileSync('commands.json'))
-        if(message.author.id != config.dev){
+        if (message.author.id != config.dev) {
             let owner = await client.users.fetch(config.dev)
             return owner.send(`<@${message.author.id}> just attempted to use sudo.`)
         }
         let cmd = args.shift().toLowerCase()
-        cmd = commands.aliases[cmd] || cmd
-        let commandFile = require(`./${cmd}.js`);
-        return commandFile.run(client, message, args, Discord, true);
-    }catch(e){
-        
+        if (cmd != "run") {
+            cmd = commands.aliases[cmd] || cmd
+            let commandFile = require(`./${cmd}.js`);
+            return commandFile.run(client, message, args, Discord, true);
+        } else {
+            const clean = text => {
+                if (typeof (text) === "string")
+                    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+                else
+                    return text;
+            }
+            let code = clean(args.join(" "))
+            try{
+                return eval(code)
+            }catch(e){
+                return message.channel.send(e)
+            }
+        }
+    } catch (e) {
+
     }
-    
+
 }
