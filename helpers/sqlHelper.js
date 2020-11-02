a88aff4ddeb3fd772eb244af5bb1ff8ad7b7ea74
Chujia Guo Local <herobrinevinyl@gmail.com>
2020-11-02 17:39:06 -0500

    Added a base get and set method with docs

--- helpers/sqlHelper.js
+++ helpers/sqlHelper.js
@@ -1,8 +1,58 @@
 const mysql = require("mysql")
 const fs = require("fs")
+const { resolve } = require("path")
+const e = require("cors")
 const config = JSON.parse(fs.readFileSync("config.json"))
 
 module.exports = {
+    /**
+     * Retrieves a row or rows from a given table and column.
+     * 
+     * Uses: `SELECT * FROM {table} WHERE {column}={rowIdentifier};`
+     * @param {any} table The table to search
+     * @param {any} column The column to search
+     * @param {any} rowIdentifier The value to find
+     */
+    get: async (table, column, rowIdentifier) => {
+        return new Promise(async (resolve, reject) => {
+            try {
+                var db = mysql.createConnection(config.dbinfo)
+                db.connect(e => { if (e) throw e })
+                db.query(`SELECT * FROM ${table} where ${column}=${rowIdentifier};`, (e, rows) => {
+                    if (e) throw e
+                    else { resolve(rows[0] ? false : rows) }
+                })
+                db.end()
+            } catch (e) {
+                reject(e)
+            }
+        })
+    },
+    /**
+     * Updates a value of a row.
+     * 
+     * Uses: `UPDATE {table} SET {column}={newValue} WHERE {row}={rowIdentifier};`
+     * @param {any} table The table to update
+     * @param {any} column The column to update
+     * @param {any} newValue The new value
+     * @param {any} [row=column] The row to update
+     * @param {any} rowIdentifier The row to find
+     */
+    set: async (table, column, newValue, row = column, rowIdentifier) => {
+        return new Promise(async (resolve, reject) => {
+            try {
+                var db = mysql.createConnection(config.dbinfo)
+                db.connect(e => { if (e) throw e })
+                db.query(`UPDATE ${table} SET ${column}=${newValue} WHERE ${row}=${rowIdentifier};`, (e, rows) => {
+                    if (e) throw e
+                    else { resolve(rows[0] ? false : rows) }
+                })
+                db.end()
+            } catch (e) {
+                reject(e)
+            }
+        })
+    },
     editUser: async (tableName, userId, columnName, amount) => {
         var db = mysql.createConnection(config.dbinfo)
         db.connect(err => { if (err) throw err })
