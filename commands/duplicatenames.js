7edff73dc3d9fb023b65c2357ab03195b0da57be
Chujia Guo Local <herobrinevinyl@gmail.com>
2020-11-02 19:33:33 -0500

    Used wrong code version

--- commands/duplicatenames.js
+++ commands/duplicatenames.js
@@ -3,12 +3,12 @@ const fs = require('fs')
 exports.run = async (client, message, args, Discord, sudo = false) => {
     const config = JSON.parse(fs.readFileSync('config.json'))
     let allMembers = message.guild.members.cache.filter(u => u.nickname && (u.roles.cache.has(config.roles.general.raider) || u.roles.cache.has(config.roles.general.eventraider))).map(m => m)
-    let nameCounter = {}
-    let duplicatenames = []
-    let duplicateMembers = []
+    var nameCounter = {}
+    var duplicatenames = []
+    var duplicateMembers = []
     for (var i in allMembers) {
         let member = allMembers[i]
-        let memberNames = getNames(member)
+        let memberNames = await getNames(member)
         for (var n in memberNames) {
             let name = memberNames[n]
             if (nameCounter[name]) {
@@ -18,16 +18,15 @@ exports.run = async (client, message, args, Discord, sudo = false) => {
             }
         }
     }
-    for (var n in memberNames) {
-        if (memberNames[n] > 1) {
+    for (var n in nameCounter) {
+        if (nameCounter[n] > 1) {
             duplicatenames.push(n)
         }
     }
     for (var n in duplicatenames) {
-        let members = allMembers.find(m => m.nickname.replace(/[a-z|]/gi, "").toLowerCase().split("|").includes(duplicatenames[n].toLowerCase()))
-        duplicateMembers.concat(members)
+        let members = allMembers.filter(m => m.nickname.replace(/[^a-z|]/gi, "").toLowerCase().split("|").includes(duplicatenames[n].toLowerCase()))
+        duplicateMembers = duplicateMembers.concat(members)
     }
-
     let duplicateEmbed = new Discord.MessageEmbed()
         .setColor("#30ffea")
         .setDescription(`The following people have duplicate names and have either the <@&${config.roles.general.raider}> role or the <@&${config.roles.general.eventraider}> role:\n${duplicateMembers.join(", ")}`)
@@ -35,9 +34,7 @@ exports.run = async (client, message, args, Discord, sudo = false) => {
 
     async function getNames(member) {
         if (member.nickname) {
-            return member.nickname.replace(/[a-z|]/gi, "").toLowerCase().split("|")
-        } else {
-            return []
+            return member.nickname.replace(/[^a-z|]/gi, "").toLowerCase().split("|")
         }
     }
 }
