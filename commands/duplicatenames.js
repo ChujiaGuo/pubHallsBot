3c4fb3db81899931431af7f47e2847ecda4a05d0
Chujia Guo Local <herobrinevinyl@gmail.com>
2020-11-02 19:31:04 -0500

    Faster duplicatenames

--- commands/duplicatenames.js
+++ commands/duplicatenames.js
@@ -3,21 +3,41 @@ const fs = require('fs')
 exports.run = async (client, message, args, Discord, sudo = false) => {
     const config = JSON.parse(fs.readFileSync('config.json'))
     let allMembers = message.guild.members.cache.filter(u => u.nickname && (u.roles.cache.has(config.roles.general.raider) || u.roles.cache.has(config.roles.general.eventraider))).map(m => m)
-    let allNames = message.guild.members.cache.filter(u => u.nickname && (u.roles.cache.has(config.roles.general.raider) || u.roles.cache.has(config.roles.general.eventraider))).map(m => m.nickname.toLowerCase().replace(/[^a-z|]/gi,"").split("|"))
-    allNames = allNames.flat()
-    let uniqueNames = [... new Set(allNames)]
-    for(var i in uniqueNames){
-        allNames.splice(allNames.indexOf(uniqueNames[i]),1)
+    let nameCounter = {}
+    let duplicatenames = []
+    let duplicateMembers = []
+    for (var i in allMembers) {
+        let member = allMembers[i]
+        let memberNames = getNames(member)
+        for (var n in memberNames) {
+            let name = memberNames[n]
+            if (nameCounter[name]) {
+                nameCounter[name] = nameCounter[name] + 1
+            } else {
+                nameCounter[name] = 1
+            }
+        }
     }
-    allNames = [... new Set(allNames)]
-    var dupeUsers = ""
-    for(var i in allNames){
-        let dupes = allMembers.filter(m => m.nickname.toLowerCase().replace(/[^a-z|]/gi,"").split("|").includes(allNames[i]))
-        dupes = dupes.map(m => `<@!${m.id}>`).join(", ")  
-        dupeUsers += dupes
+    for (var n in memberNames) {
+        if (memberNames[n] > 1) {
+            duplicatenames.push(n)
+        }
     }
+    for (var n in duplicatenames) {
+        let members = allMembers.find(m => m.nickname.replace(/[a-z|]/gi, "").toLowerCase().split("|").includes(duplicatenames[n].toLowerCase()))
+        duplicateMembers.concat(members)
+    }
+
     let duplicateEmbed = new Discord.MessageEmbed()
         .setColor("#30ffea")
-        .setDescription(`The following people have duplicate names and have either the <@&${config.roles.general.raider}> role or the <@&${config.roles.general.eventraider}> role:\n${dupeUsers}`)
+        .setDescription(`The following people have duplicate names and have either the <@&${config.roles.general.raider}> role or the <@&${config.roles.general.eventraider}> role:\n${duplicateMembers.join(", ")}`)
     await message.channel.send(duplicateEmbed)
+
+    async function getNames(member) {
+        if (member.nickname) {
+            return member.nickname.replace(/[a-z|]/gi, "").toLowerCase().split("|")
+        } else {
+            return []
+        }
+    }
 }
