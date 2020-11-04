1d5574a3f5135bc7ada610101447524c601a4407
Chujia Guo Local <herobrinevinyl@gmail.com>
2020-11-04 15:36:13 -0500

    Error Logs command

---
+++ commands/errorlogs.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const config = JSON.parse(fs.readFileSync("config.json"))
+
+exports.run = async (client, message, args, Discord) => {
+    let errorLog = fs.readFileSync(`${config.errorLog}`).toString()
+
+    args = args[0] || 15
+    args = Math.abs(args)
+    
+    let linesRead = errorLog.split("\n").slice(-args).join('\n')
+    let errorEmbed = new Discord.MessageEmbed()
+        .setColor("#30ffea")
+        .setDescription(`Last ${args} lines of error:\n${linesRead}`)
+        .setTimestamp()
+    await message.author.send(errorEmbed)
+}
