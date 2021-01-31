0a7cba0b57bf81134f8a6804545f8e01df0cdfd5
Chujia Guo Local <herobrinevinyl@gmail.com>
2021-01-30 23:25:26 -0500

    Added extra header

--- commands/parsecharactersv2.js
+++ commands/parsecharactersv2.js
@@ -59,7 +59,8 @@ module.exports = {
                 var options = {
                     proxy: 'http://Nashex:Goldilocks1@us-wa.proxymesh.com:31280',
                     headers: {
-                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'
+                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
+                        'Proxy-Authorization' : 'Basic TmFzaGV4OkdvbGRpbG9ja3Mx'
                     }
                 };
 
