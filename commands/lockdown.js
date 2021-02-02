65e8f008c3140783fa86bdde376bbd3968c66ce2
chujia guo <herobrinevinyl@gmail.com>
2021-02-01 23:22:32 -0500

    Lockdown command

---
+++ commands/lockdown.js
@@ -0,0 +1,61 @@
+const fs = require('fs')
+
+exports.run = async (client, message, args, Discord) => {
+    const config = JSON.parse(fs.readFileSync('config.json'))
+    var lockdownLogs = JSON.parse(fs.readFileSync('lockdownPerms.json'))
+
+    return new Promise(async (resolve, reject) => {
+
+        let channel = message.channel//Get channel permissions
+        if (lockdownLogs[channel.id]) { //If channel currently exists in lockdown, remove lockdown
+            try {
+                await channel.overwritePermissions(lockdownLogs[channel.id], "Unlocking a channel") //Give back previous permissions
+                delete lockdownLogs[channel.id] //Remove from logs
+                fs.writeFileSync('lockdownPerms.json', JSON.stringify(lockdownLogs))
+                let confirmationEmbed = new Discord.MessageEmbed()
+                    .setColor("#30ffea")
+                    .setAuthor("This channel has been unlocked.")
+                message.channel.send(confirmationEmbed)
+                resolve(true)
+            } catch (e) {
+                reject(false)
+                await message.channel.send(`${channel} could not be locked down for the following reason:\n\`\`\`${e}\`\`\``)
+            }
+
+        } else { //Else add to lockdown
+            try {
+                let allPermissions = channel.permissionOverwrites // Get all channel permissions
+
+                lockdownLogs[channel.id] = allPermissions //Add permissions to logs
+                fs.writeFileSync('lockdownPerms.json', JSON.stringify(lockdownLogs))
+
+                //Remove "SEND_MESSAGES" permission from every role except higher ups
+                let { hdev, hrl, officer, mod, admin } = config.roles.staff //Get higher up ids
+                let higherUps = [hdev, hrl, officer, mod, admin]
+
+                for (r of allPermissions) {
+                    if (!higherUps.includes(r[0])) {
+                        let permission = r[1]
+                        await permission.update({
+                            SEND_MESSAGES: false
+                        }, "Locking down the channel.")
+                    }
+                }
+                await channel.updateOverwrite(client.user.id, {
+                    SEND_MESSAGES: true,
+                    MANAGE_CHANNELS: true,
+                })
+                let confirmationEmbed = new Discord.MessageEmbed()
+                    .setColor("#30ffea")
+                    .setAuthor("This channel has been locked down.")
+                message.channel.send(confirmationEmbed)
+                resolve(true)
+
+            } catch (e) {
+                reject(false)
+                await message.channel.send(`${channel} could not be locked down for the following reason:\n\`\`\`${e}\`\`\``)
+            }
+        }
+    })
+
+}
